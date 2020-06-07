7b31261e3129710f888f1fe598985f7ef41c2110
İbrahim Uzuğ <ibrahimuzug7@gmail.com>
2020-06-07 12:56:53 +0300

    Initial Commit Of Shopping Application

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
